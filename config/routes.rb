dd0828d802a6a3a50c6be261d3bc512e2dfde24a
Isabelle Ferrier <ferrierisabelle@gmail.com>
2022-04-11 11:57:25 +0200

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root to: 'tasks#index'
-  get '/index', to: 'tasks#index', as: :tasks
-  get 'tasks/new', to: 'tasks#new', as: :task_new
-  post '/index', to: 'tasks#create'
-  get '/tasks/:id', to: 'tasks#show', as: :task
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
-  patch 'tasks/:id', to: 'tasks#update'
-  delete 'tasks/:id', to: 'tasks#destroy', as: :delete
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
